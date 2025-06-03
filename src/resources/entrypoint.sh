9d3d6cf0760bc6d8f78fc62abda1a104046852cd
Hauke Heidemeyer <HHeidemeyer@ukaachen.de>
2025-06-03 12:17:49 +0200

    chore: Move configuration to /usr/share

--- src/resources/entrypoint.sh
+++ src/resources/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 CONFIG_FILE=/etc/aktin/aktin.properties
-DEFAULT_FILE=/opt/default-aktin.properties
+# Default config file, used to recover missing aktin.properties
+DEFAULT_FILE=/usr/share/aktin/default-aktin.properties
 
 # Copy default only if missing
 if [ ! -f "$CONFIG_FILE" ]; then
     echo "No aktin.properties found, copying default configuration..."
     cp "$DEFAULT_FILE" "$CONFIG_FILE"
+    ln -sf "$CONFIG_FILE" /opt/wildfly/standalone/configuration/aktin.properties
 fi
 
 # Start the application

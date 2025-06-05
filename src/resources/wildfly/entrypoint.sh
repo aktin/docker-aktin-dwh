f8dec35c6cafcd58a146d9ac32f760d4e3f44477
akomii <alexkombie@gmail.com>
2025-06-05 15:25:32 +0200

    some cleanup post merge

--- src/resources/wildfly/entrypoint.sh
+++ src/resources/wildfly/entrypoint.sh
@@ -2,16 +2,14 @@
 #--------------------------------------
 # Script Name:  entrypoint.sh
 # Author:       akombeiz@ukaachen.de, hheidemeyer@ukaachen.de
-# Purpose:      Configures the WildFly server for the AKTIN emergency department system.
+# Purpose:      Selects and restores configuration file for the AKTIN emergency department system.
 #--------------------------------------
 
 set -e
-CONFIG_FILE=/etc/aktin/aktin.properties
-# Default config file, used to recover missing aktin.properties
-DEFAULT_FILE=/usr/share/aktin/default-aktin.properties
 
-DEV_PROPERTIES=/usr/share/aktin/dev-aktin.properties
 PROD_PROPERTIES=/etc/aktin/aktin.properties
+DEV_PROPERTIES=/usr/share/aktin/dev-aktin.properties
+DEFAULT_PROPERTIES=/usr/share/aktin/default-aktin.properties
 WILDLFY_PROPERTIES=/opt/wildfly/standalone/configuration/aktin.properties
 
 if [ "$DEV_MODE" = "true" ]; then
@@ -19,13 +17,12 @@ if [ "$DEV_MODE" = "true" ]; then
   ln -sf "${DEV_PROPERTIES}" "${WILDLFY_PROPERTIES}"
 else
   echo "Running in PROD mode"
+  # Copy default only if missing
+  if [ ! -f "${PROD_PROPERTIES}" ]; then
+      echo "No aktin.properties found, copying default configuration..."
+      cp "${DEFAULT_PROPERTIES}" "${PROD_PROPERTIES}"
+  fi
   ln -sf "${PROD_PROPERTIES}" "${WILDLFY_PROPERTIES}"
-# Copy default only if missing
-if [ ! -f "$CONFIG_FILE" ]; then
-    echo "No aktin.properties found, copying default configuration..."
-    cp "$DEFAULT_FILE" "$CONFIG_FILE"
-    ln -sf "$CONFIG_FILE" /opt/wildfly/standalone/configuration/aktin.properties
 fi
 
-# Start the application
 exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0
